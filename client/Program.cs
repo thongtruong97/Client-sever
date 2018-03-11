63432c6fba1fe51e78e62780d962c371c2f374d2
thong <tqt15111997@gmail.com>
2018-03-11 21:42:42 +0700

    abcbcbjj

---
+++ client/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Text;
+using System.Net.Sockets;
+using System.IO;
+
+namespace client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint ipServer = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 1234);
+            client.Connect(ipServer);
+            string cGui = "Send File Text1";
+            byte[] gui = Encoding.ASCII.GetBytes(cGui);
+            client.Send(gui);
+            byte[] nhan = new byte[1024 * 60];
+            int rec = client.Receive(nhan);
+            string cNhan = Encoding.ASCII.GetString(nhan, 0, rec);
+            StreamWriter sw = new StreamWriter("..//..//TextFileClient.txt");
+            sw.Write(cNhan);
+            sw.Close();
+        }
+    }
+}
---
+++ sever/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Text;
+using System.Net.Sockets;
+using System.IO;
+namespace sever
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint ip = new IPEndPoint(IPAddress.Loopback, 1234);
+            server.Bind(ip);
+            StreamReader sr = new StreamReader("../../TextFileSever.txt");
+            string readFile = sr.ReadToEnd();
+            sr.Close();
+            server.Listen(4);
+            Socket client = server.Accept();
+            byte[] nhan = new byte[1024 * 60];
+            int rec = client.Receive(nhan);
+            string sNhan = Encoding.ASCII.GetString(nhan, 0, rec);
+            Console.WriteLine("Client gui: " + sNhan);
+            byte[] gui = Encoding.ASCII.GetBytes(readFile);
+            client.Send(gui);
+        }
+    }
+}
